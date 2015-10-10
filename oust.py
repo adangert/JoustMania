1ef7782b9532a30e58d14027952c0e619ef2c4a3
Aaron Angert <aangertdev@gmail.com>
2015-10-10 00:57:07 +0000

    added back beginning of game, FFA ending still broken

--- oust.py
+++ oust.py
@@ -458,3 +458,98 @@ def Teams():
                 for serial, move in controllers_alive.items():
                     if serial not in available:
                         del controllers_alive[serial]
+
+
+while True:
+    start_ffa = False
+    start_teams = False
+    while True:
+        for move in moves:
+            if move.this == None:
+                print "Move initialisation failed, reinitialising"
+                moves = []
+                break
+
+            # If a controller is plugged in over USB, pair it and turn it white
+            # This appears to occasionally kernel panic raspbian!
+            if move.connection_type == psmove.Conn_USB:
+                if move.get_serial() not in paired_controllers:
+                    pair.equal_pair(move)
+                    paired_controllers.append(move.get_serial())
+
+                move.set_leds(255,255,255)
+                move.update_leds()
+                continue
+
+            if move.poll():
+                # If the trigger is pulled, join the game
+                if move.get_serial() not in controllers_alive:
+                    if move.get_trigger() > 100:
+                        controllers_alive[move.get_serial()] = move
+
+                if move.get_serial() in controllers_alive:
+                    if move.get_serial() not in controller_teams:
+                        move.set_leds(255,255,255)
+                    else:
+                        move.set_leds(*team_colors[controller_teams[move.get_serial()][0]])
+                else:
+                    move.set_leds(0,0,0)
+
+                if move.get_buttons() == 0:
+                    if move.get_serial() in controller_teams:
+                        controller_teams[move.get_serial()][1] = True                
+
+                # Triangle starts the game early
+                if move.get_buttons() == 524288:
+                    change_team(move)
+
+                # Triangle starts the game early
+                if move.get_buttons() == 16:
+                    start_ffa = True
+
+                #print move.get_buttons()
+                # Triangle starts the game early
+                if move.get_buttons() == 128:
+                    start_teams = True
+
+                # Circle shows battery level
+                if move.get_buttons() == 32:
+                    battery = move.get_battery()
+
+                    if battery == 5: # 100% - green
+                        move.set_leds(0, 255, 0)
+                    elif battery == 4: # 80% - green-ish
+                        move.set_leds(128, 200, 0)
+                    elif battery == 3: # 60% - yellow
+                        move.set_leds(255, 255, 0)
+                    else: # <= 40% - red
+                        move.set_leds(255, 0, 0)
+
+                move.set_rumble(0)
+                move.update_leds()
+
+        # If we've got more/less moves, register them
+        if psmove.count_connected() != len(moves):
+            moves = [psmove.PSMove(x) for x in range(psmove.count_connected())]
+
+        # Everyone's in
+        #if (len(controllers_alive) == len(moves) and len(controllers_alive) > 0):
+        #    break
+
+
+        # Someone hit triangle
+        if (len(controllers_alive) >= 2 and start_ffa == True):
+            FFA() 
+            break
+
+        if (len(controllers_alive) >= 2 and start_teams == True):
+            check = True
+            for move in controllers_alive:
+                if move not in controller_teams:
+                    check = False
+            if check:
+                Teams()
+                break
+            else:
+                break
+

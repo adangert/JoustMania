11e8f064a34c737a68f169eeaeafc13e06356f1d
Aaron Angert <aangertdev@gmail.com>
2015-10-27 23:52:36 +0000

    added supervisor and reboot to setup.sh

--- setup.sh
+++ setup.sh
@@ -34,3 +34,7 @@ OLD="#options snd-usb-audio index=-2"
 NEW="options snd-usb-audio index=0\noptions snd_bcm2835 index=-2"
 
 sudo sed -i -e 's/$OLD/$NEW/g' /etc/modprobe.d/alsa-base.conf
+
+sudo cp -r /home/pi/PiParty/supervisor/ /etc/supervisor/
+
+sudo reboot

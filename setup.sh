7b7281aecbfea5f239f3d6d26ef77e9078c244a6
Aaron Angert <aangertdev@gmail.com>
2015-10-31 18:21:52 -0500

    Update setup.sh
    added auto yes to apt-get install

--- setup.sh
+++ setup.sh
@@ -13,14 +13,14 @@ git remote add jmacarthur https://github.com/jmacarthur/psmoveapi.git
 git fetch jmacarthur
 git cherry-pick -n e3838a5c49313b8865ff493573aa417e8e4a391b
 
-sudo apt-get install swig python-dev bluez python-pyaudio python-pip python-numpy python-setuptools libsamplerate0-dev
+sudo apt-get install -y swig python-dev bluez python-pyaudio python-pip python-numpy python-setuptools libsamplerate0-dev
 
 sudo pip install scikits.samplerate psutil
 
 git submodule init
 git submodule update
 
-sudo apt-get install build-essential cmake libudev-dev libbluetooth-dev libv4l-dev libopencv-dev
+sudo apt-get install -y build-essential cmake libudev-dev libbluetooth-dev libv4l-dev libopencv-dev
 pwd
 mkdir build
 cd build
@@ -38,7 +38,7 @@ NEW='env_keep += "PYTHONPATH"'
 
 sudo sed -i -e "s/$OLD/$NEW/g" /etc/sudoers
 
-sudo apt-get install supervisor
+sudo apt-get install -y supervisor
 
 sudo cp -r /home/pi/PiParty/supervisor/ /etc/
 

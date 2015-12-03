ea191457b3dcfada9df0e00589404dc102461ecc
Aaron Angert <aangertdev@gmail.com>
2015-12-03 01:21:40 +0000

    install psmoveapi with setup script, started piparty.py

---
+++ piparty.py
@@ -0,0 +1,10 @@
+import os
+import psmove
+
+class menu():
+    def __init__():
+        pass
+
+
+if __name__ == "__main__":
+    piparty = menu()
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#update OS
+sudo apt-get dist-upgrade -y
+sudo apt-get update -y
+cd /home/pi
+
+#install psmoveapi
+git clone https://github.com/thp/psmoveapi.git
+cd psmoveapi
+git remote add jmacarthur https://github.com/jmacarthur/psmoveapi.git
+git fetch jmacarthur
+git cherry-pick -n e3838a5c49313b8865ff493573aa417e8e4a391b
+git submodule init
+git submodule update
+mkdir build
+cd build
+cmake ..
+make -j4

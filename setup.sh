7d9b21bfc00616e8b63b0c5e808bc412ee79e576
Aaron Angert <aangertdev@gmail.com>
2016-09-12 01:34:02 +0000

    updated for jessie

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ sudo pip install psutil enum
 #install psmoveapi
 git clone https://github.com/thp/psmoveapi.git
 cd psmoveapi
-git remote add jmacarthur https://github.com/jmacarthur/psmoveapi.git
-git fetch jmacarthur
+#git remote add jmacarthur https://github.com/jmacarthur/psmoveapi.git
+#git fetch jmacarthur
 
 
-git cherry-pick -n e3838a5c49313b8865ff493573aa417e8e4a391b
+#git cherry-pick -n e3838a5c49313b8865ff493573aa417e8e4a391b
 git submodule init
 git submodule update
 mkdir build
@@ -27,11 +27,10 @@ make -j4
 #installs custom supervisor script for running piparty on startup
 sudo cp -r /home/pi/PiParty/supervisor/ /etc/
 
-#this may need to be in /lib/modprobe.d/aliases.conf instead for jessie release
 #sets up sound card as primary sound device
 OLD='options snd-usb-audio index=-2'
 NEW='options snd-usb-audio index=0\noptions snd_bcm2835 index=-2'
-sudo sed -i -e "s/$OLD/$NEW/g" /etc/modprobe.d/alsa-base.conf
+sudo sed -i -e "s/$OLD/$NEW/g" /lib/modprobe.d/aliases.conf
 
 #allows python path to be kept after sudo command
 OLD='env_reset'

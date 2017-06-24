48d8cfe98cb27c795b825d7b905f9f31dcf7e7ee
adangert <angert346@gmail.com>
2017-06-24 13:31:51 -0500

    add basic file for windows support
    This is the start of getting Joustmania working on Windows, it is not currently operational.

---
+++ setup_windows.bat
@@ -0,0 +1,14 @@
+::install choco
+::@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install python3 -y
+choco install git -y
+::refreshenv
+choco install visualstudio2015community -y
+git clone --recursive https://github.com/thp/psmoveapi.git
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=""User""' -y
+set path=C:\Program Files\CMake\bin;%path%
+choco install microsoft-build-tools -y
+pip install numpy
+choco install swig -y
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin
+psmoveapi/scripts/visualc/build_msvc_2015.bat

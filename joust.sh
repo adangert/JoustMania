7bc2aaab622f4c2d309c28bfadb85836cbcca5ab
Maciej Babinski <mbabinski@google.com>
2017-09-27 03:07:39 +0000

    Switch to using virtualenv for most python deps.   * We continue using system installs of scipy and numpy, since they are super slow to build.   * supervisor runs our scripts as root directly; using sudo makes it impossible for supervisor to kill them.   * joust.sh re-runs itself with sudo if it _wasn't_ started as root.

--- joust.sh
+++ joust.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $UID -ne 0 ]; then
+  echo "Not root. Using sudo."
+  exec sudo $0
+fi
+
 export HOME="/home/pi/JoustMania"
 export PYTHONPATH="/home/pi/psmoveapi/build/"
-sudo /usr/bin/python3.6 /home/pi/JoustMania/piparty.py
+/home/pi/JoustMania/venv/bin/python3.6 /home/pi/JoustMania/piparty.py

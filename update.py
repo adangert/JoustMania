ddba911bdf2f94df2e54431b5616af40fae454a5
Aaron Angert <aangertdev@gmail.com>
2019-04-24 00:21:31 -0500

    minor bug fix, removed comments

--- update.py
+++ update.py
@@ -1,8 +1,6 @@
 import subprocess  
 from piaudio import Audio, InitAudio
   
-#almost working, need to get it to be a button push when it's a big update.  
-  
 if __name__ == "__main__":
     InitAudio()
     check_for_update() 
@@ -26,9 +24,6 @@ def big_update():
     run_command("git pull")
     run_command("sudo /home/pi/JoustMania/setup.sh")
     Audio('audio/Menu/joustmania_updated.wav').start_effect_and_wait()
-    #audio, big update started
-    #do big update
-    #say big update completed
     
 def check_for_update():
     process = run_command("git fetch")
@@ -40,16 +35,12 @@ def check_for_update():
         print('big updtae available')
         Audio('audio/Menu/large_update.wav').start_effect_and_wait()
         return True
-        #audio cue here
-        #run_command("sudo /home/pi/JoustMania/setup.sh")
 
-    elif (len(diff_files) > 1):
+    elif (len(diff_files) >= 1):
         print("doing small pull")
         pull = run_command("git pull")
         Audio('audio/Menu/joustmania_updated.wav').start_effect_and_wait()
         return False
-
-        #Audio('audio/Menu/tradeoff2.wav').start_effect_and_wait()
     
     
     

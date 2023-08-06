5027fa0924652dcdfb5319cc0e9eb34a100e910e
Aaron Angert <aangertdev@gmail.com>
2023-08-06 17:24:21 -0400

    updated kill script with -9 and [p] to remove grep

--- kill_processes.sh
+++ kill_processes.sh
@@ -3,4 +3,4 @@
 #this is for development purposes only, to stop the automattically
 #running piparty scripts
 sudo supervisorctl stop joustmania
-sudo kill $(ps aux | grep 'piparty' | awk '{print $2}')
+sudo kill -9 $(ps aux | grep '[p]iparty' | awk '{print $2}')

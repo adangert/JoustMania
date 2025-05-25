f8e8112c33b3912fa50d680ed816bebd409ea86e
jerkey <jerkey@g-cipher.net>
2025-05-25 01:18:37 -0700

    Update disable_internal_bluetooth.sh for debian 12 and later

--- disable_internal_bluetooth.sh
+++ disable_internal_bluetooth.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 # this will disable on-board bluetooth
-#this will allow only class one long range btdongles to connect to psmove controllers
-sudo echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
-sudo systemctl disable hciuart
+# this will allow only class one long range btdongles to connect to psmove controllers
 
-#remove onboard bluetooth folders
+DIST_REL=$(cut -f2 <<< $(lsb_release -r))
+if [ "$DIST_REL" -ge 12 ]; then
+	echo "the distribution $DIST_REL is larger than 12"
+	config_loc=/boot/firmware/config.txt || exit -1
+else
+	echo "the distribution is smaller than 12"
+	config_loc=/boot/config.txt || exit -1
+fi
+
+echo "dtoverlay=disable-bt" | sudo tee -a $config_loc
 sudo rm -rf /var/lib/bluetooth/*
+sudo systemctl disable hciuart
 
 sudo reboot
